6aa903b7deba2ab42abf0f3e7b42b3b520fb418b
MinhajLahith2002 <minhajse2023@gmail.com>
2025-07-28 12:01:04 +0530

    booking confirmation email added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,8 @@ import { Inngest } from "inngest"
 import User from '../models/User.js'
 import Booking from '../models/Booking.js'
 import Show from '../models/Show.js'
+import { model } from "mongoose";
+import sendEmail from "../configs/nodeMailer.js";
 
 
 // Create a client to send and receive events
@@ -54,9 +56,9 @@ const syncUserUpdation = inngest.createFunction(
 //inngest function to cancel booking and release seats of show after 10 mins of
 //booking created if payment is not made
 const releaseSeatsAndDeleteBooking = inngest.createFunction(
-  {id: 'release-seats-delete-booking'},
-  {event: "app/checkpayment"},
-  async ({event, step}) => {
+  { id: 'release-seats-delete-booking' },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
     const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
     await step.sleepUntil('wait-for-10-minutes', tenMinutesLater);
 
@@ -67,7 +69,7 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
       // if payment is not made, release seats and delete booking
       if (!booking.isPaid) {
         const show = await Show.findById(booking.show);
-        booking.bookedSeats.forEach((seat)=>{
+        booking.bookedSeats.forEach((seat) => {
           delete show.occupiedSeats[seat]
         });
         show.markModified('occupiedSeats')
@@ -78,9 +80,40 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
   }
 )
 
+//inngest function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+
+    const booking = await Booking.findById(bookingId).populate({
+      path: 'show',
+      populate: { path: "movie", model: "Movie" }
+    }).populate('user');
+
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+      body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                <h2>Hi ${booking.user.name}, </h2>
+                <p>Your booking for <strong style="color: #F84565;">
+                "${booking.show.movie.title}"</strong> is confirmed.</p>
+                <p>
+                    <strong>Date:</strong> 
+                    ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/ Kolkata' })}<br/>
+                    < strong > Time:</strong > ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+               </p >
+               <p>Enjoy the show! 🍿</p>
+               <p>Thanks for booking with us!<br/>- QuickShow Team</p>
+            </div > `
+    })}
+)
+
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
-  releaseSeatsAndDeleteBooking
+  releaseSeatsAndDeleteBooking,
+  sendBookingConfirmationEmail
 ];
